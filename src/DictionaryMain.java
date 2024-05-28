791945bd5db1f8ebace36c50df9d6d595be75387
nikol <nikolaysem2004@gmail.com>
2024-05-29 02:53:05 +0300

    главный класс(пустой)

---
+++ src/DictionaryMain.java
@@ -0,0 +1,20 @@
+
+public class DictionaryMain {
+
+	public static void main(String[] args) {
+		/*
+		 * interface Dictionary
+		 * 
+		 * abstract class Dictionary
+		 * class NumsDict extends Dictionary
+		 * class LatinDict extends Dictionary
+		 * 
+		 * class FileOperations
+		 * class DictionaryMain
+		 * 
+		 * 
+		 * 
+		 * */
+	}
+	
+}
